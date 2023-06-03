5d5711dc0965cc5d881ce6226a57a02627c9cc19
= <=>
2023-06-03 14:53:19 +0530

    fixed small issues like : github btn , right sidebar update , copyright date etc

--- components/Navbar.js
+++ components/Navbar.js
@@ -11,8 +11,8 @@ export default function Navbar(){
                         <Link href = "/"><Image unoptimized={true} src = "/images/logo.png" alt="Muzical Logo" height={100} width={100} className = "w-[5rem] sm:w-[8rem] " /></Link>
                 </div>
                 <div className = "navbar_right flex space-x-5">
-                    <Image src = "/dark-moon.svg" alt = "dark theme logo" height = {100} width = {100} className = "sm:w-[1.5rem] w-[1rem] cursor-pointer " />
-                    <Image src = "/dark-github.svg" alt = "dark theme logo" height = {100} width = {100} className = "sm:w-[1.5rem] w-[1rem] cursor-pointer" />                    
+                    {/* <Image src = "/dark-moon.svg" alt = "dark theme logo" height = {100} width = {100} className = "sm:w-[1.5rem] w-[1rem] cursor-pointer " /> */}
+                    <Link target="_blank" href="https://github.com/anand346/findissues" ><Image src = "/dark-github.svg" alt = "dark theme logo" height = {100} width = {100} className = "sm:w-[1.5rem] w-[1rem] cursor-pointer" /></Link>
                 </div>
             </div>
         </>
