2427e5637ecfb4eec4105c04cae98031e50a053b
Anand Raj <64061582+anand346@users.noreply.github.com>
2023-06-06 23:48:42 +0530

    Update RightSidebar.js

--- components/RightSidebar.js
+++ components/RightSidebar.js
@@ -7,10 +7,10 @@ export default function RightSidebar(){
         <>
             <div className = {` ${styles.right_sidebar} hidden  md:block overflow-auto right_sidebar sm:p-8 p-7 w-[23%] border-l-2 border-main_secondary p-8 h-full`}>
                 <div className = "right_sidebar_content flex flex-col justify-start items-start">
-                    <p className = "right_sidebar_title text-main_primary font-semibold italic text-[16px] lg:text-[18px] mb-4">How To Use ?</p>
+                    <p className = "right_sidebar_title text-main_primary font-semibold italic text-[16px] lg:text-[18px] mb-4">How To Use?</p>
                     {/* <p className = "right_sidebar_title text-main_primary font-semibold italic text-[14px] mb-4">To find the most recent issues created on GitHub of your favourite programming lang. and which is not assigned to anyone till now. 
                     </p> */}
-                    {/* <p className = "right_sidebar_title text-main_primary font-semibold italic text-[12px] lg:text-[14px] mb-4">To find most recent created issues on Github follow these steps  :</p> */}
+                    {/* <p className = "right_sidebar_title text-main_primary font-semibold italic text-[12px] lg:text-[14px] mb-4">To find most recent created issues on Github follow these steps :</p> */}
                     <ol className = "list-decimal ml-[13px] mb-3">
                         <li className = "right_sidebar_title text-main_primary font-semibold italic text-[12px] lg:text-[14px] mb-3">
                         Please select your preferred programming language from the categories listed in the sidebar on the left.
