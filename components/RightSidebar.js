267607d5cedb51f2061043721a11054ab0c1ae31
Krishna Gupta <krishna.ghhh@gmail.com>
2023-06-06 15:54:49 +0530

    right side bar updated

--- components/RightSidebar.js
+++ components/RightSidebar.js
@@ -7,16 +7,16 @@ export default function RightSidebar(){
         <>
             <div className = {` ${styles.right_sidebar} overflow-auto right_sidebar sm:p-8 p-7 w-[23%] border-l-2 border-main_secondary p-8 h-full`}>
                 <div className = "right_sidebar_content flex flex-col justify-start items-start">
-                    <p className = "right_sidebar_title text-main_primary font-semibold italic text-[18px] mb-4">How it works ?</p>
+                    <p className = "right_sidebar_title text-main_primary font-semibold italic text-[18px] mb-4">How it works?</p>
                     {/* <p className = "right_sidebar_title text-main_primary font-semibold italic text-[14px] mb-4">To find the most recent issues created on GitHub of your favourite programming lang. and which is not assigned to anyone till now. 
                     </p> */}
-                    <p className = "right_sidebar_title text-main_primary font-semibold italic text-[14px] mb-4">To find most recent created issues on Github follow these steps  :</p>
+                    <p className = "right_sidebar_title text-main_primary font-semibold italic text-[14px] mb-4">To find most recent created issues on Github follow these steps :</p>
                     <ol className = "list-decimal ml-[13px]">
                         <li className = "right_sidebar_title text-main_primary font-semibold italic text-[14px] mb-3">
-                        Choose your fav programming lang from basic search tab on the left side bar .
+                        Please select your preferred programming language from the categories listed in the sidebar on the left.
                         </li>
                         <li className = "right_sidebar_title text-main_primary font-semibold italic text-[14px] mb-3">
-                        This will show you all the recent created issues on GitHub of that lang. that are not assigned to anyone .
+                        It will display 30 of the most recent Github issues created in that language that have not yet been assigned to anyone.
                         </li>
                         <li className = "right_sidebar_title text-main_primary font-semibold italic text-[14px] mb-3">
                         If you want to filter issues based on stars and forks of repo then you can use advance search .
