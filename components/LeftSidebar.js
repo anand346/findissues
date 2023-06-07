546471d5eef840ed14f114f4a5da0fc8a1bc8c97
Vivek Sondhiya <viveksondhiya555@gmail.com>
2023-06-07 10:46:11 +0530

    added space

--- components/LeftSidebar.js
+++ components/LeftSidebar.js
@@ -20,7 +20,7 @@ export default function LeftSidebar(){
                 <div className = "left_sidebar_content flex flex-col justify-start items-start">
                     <div className = "basic_search w-full flex flex-col items-start justify-start md:mb-5">
                         <p className = "basic_search_title w-full mb-2 md:mb-4 text-main_primary font-semibold italic text-[16px] lg:text-[18px]">Basic Search</p>
-                        <div className = {` basic_search_content w-full flex items-start justify-start flex-wrap space-y-3`}>
+                        <div className = {` basic_search_content w-full flex items-start justify-start flex-wrap space-y-3 `}>
                             {
                                 langs.map(lang => {
                                     return (
