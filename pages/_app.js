f13acf47ce1fda574227b57fb2e33bf5c862be7a
= <=>
2023-09-19 18:33:24 +0530

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,22 @@ export default function App({ Component, pageProps }) {
         </GeneralLayout>
       </ThemeProvider>
       
-      
+      <Script
+        id="googleAnalytics"
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload" id="google_analytics" >
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+            });
+        `}
+      </Script>
       <Script src = " https://unpkg.com/flowbite@1.5.1/dist/flowbite.js" /> 
     </>
   )
