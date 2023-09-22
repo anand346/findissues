9ce96c66a93c2ace5085a65a0d46cf0bd182fae8
Rupali Haldiya <rupali7487@gmail.com>
2023-09-22 14:10:11 +0530

    fix: show invalid error message

--- pages/search/[lang].js
+++ pages/search/[lang].js
@@ -5,6 +5,7 @@ import IssuesCard from "@/components/IssuesCard";
 import moment from "moment/moment";
 import { SkeletonCard } from "@/components/SkeletonCard";
 import { priority_langs } from "@/helper/priority_langs";
+import { langs } from "@/helper/Languages";
 
 export default function Search({allIssues}){
 
@@ -23,13 +24,19 @@ export default function Search({allIssues}){
     return (
         <>
             <div className = {`${styles.landing_main} p-3 md:p-8 issues_result overflow-auto w-[100%] md:w-[54%] landing_main h-full flex flex-col items-start justify-start`}>
-                <p className = "w-[250px] mb-4 font-semibold text-[16px] lg:text-[18px] text-main_primary"><span className="inline-block italic">All Unassigned Issues</span> 👇</p>
+                {
+                    allIssues.length ? <>
+                        <p className = "w-[250px] mb-4 font-semibold text-[16px] lg:text-[18px] text-main_primary"><span className="inline-block italic">All Unassigned Issues</span> 👇</p>
                 {
                     allIssues?.map(issue => {
                        return (
                             <IssuesCard key={issue.issueId} issue={issue} />
                        ) 
                     })
+                }
+                    </> : <>
+                    <p className = "w-full mb-4 font-semibold text-[16px] lg:text-2xl text-main_primary text-center">Invalid search query :(</p>
+                    </>
                 }
             </div>
         </>
@@ -72,42 +79,45 @@ function getTimeFromNow(created_at){
 
 async function loadIssues(url,query_lang){
 
-    
-    const issues_res = await fetch(url,{
-        headers: {
-            'Authorization' : "token "+process.env.NEXT_PUBLIC_ACCESS_TOKEN,
-            'Accept' : 'application/vnd.github.v3+json'
-        }
-      });
-
-    const issues_json = await issues_res.json();
-    const issueItems = issues_json.items;
-
     var allIssues = [];
+    const matching_langs = langs.filter(lang => lang.query === query_lang)
 
-    var repo_res = await loadRepo(issueItems);
+    if(matching_langs.length > 0)
+    {
+        const issues_res = await fetch(url,{
+            headers: {
+                'Authorization' : "token "+process.env.NEXT_PUBLIC_ACCESS_TOKEN,
+                'Accept' : 'application/vnd.github.v3+json'
+            }
+        });
 
-    issueItems.forEach(issue => {
+        const issues_json = await issues_res.json();
+        const issueItems = issues_json.items;
+
+        var repo_res = await loadRepo(issueItems);
+
+        issueItems.forEach(issue => {
+            
         
-       
-        var finalTimeFromNow = getTimeFromNow(issue.created_at);
-        var lang = query_lang;
+            var finalTimeFromNow = getTimeFromNow(issue.created_at);
+            var lang = query_lang;
 
-        var issueObj = {
-            issueId : issue.id,
-            issueNumber : issue.number,
-            issueUrl : issue.html_url,
-            issueTitle : issue.title,
-            repoTitle : repo_res[issue.id].full_name,
-            timeFromNow : finalTimeFromNow,
-            repo_forks : repo_res[issue.id].forks_count,
-            repo_stars : repo_res[issue.id].stargazers_count,
-            language : query_lang
-        }
+            var issueObj = {
+                issueId : issue.id,
+                issueNumber : issue.number,
+                issueUrl : issue.html_url,
+                issueTitle : issue.title,
+                repoTitle : repo_res[issue.id].full_name,
+                timeFromNow : finalTimeFromNow,
+                repo_forks : repo_res[issue.id].forks_count,
+                repo_stars : repo_res[issue.id].stargazers_count,
+                language : query_lang
+            }
 
-        allIssues.push(issueObj);
-        
-    });
+            allIssues.push(issueObj);
+            
+        });
+    }
 
     // setIssues(allIssues);
     return allIssues;
