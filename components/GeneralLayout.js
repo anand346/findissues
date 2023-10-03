e2c7fcc5e137a7d3cd8e38912316057a54404d02
Harsh97x <Rynzie@protonmail.com>
2023-10-03 14:29:02 +0530

    added title, formatted code with prettier for better readability

--- components/GeneralLayout.js
+++ components/GeneralLayout.js
@@ -2,12 +2,11 @@ import React from "react";
 import Navbar from "./Navbar";
 import MainLayout from "./MainLayout";
 
-export default function GeneralLayout({children}){
-    return (
-        <div className = "general_layout w-screen h-screen flex flex-col justify-center items-center ">
-          <Navbar />
-          <MainLayout childrenEl={children} />
-        </div>
-    )
-
+export default function GeneralLayout({ children }) {
+  return (
+    <div className="general_layout w-screen h-screen flex flex-col justify-center items-center ">
+      <Navbar />
+      <MainLayout childrenEl={children} />
+    </div>
+  );
 }
--- components/MainLayout.js
+++ components/MainLayout.js
@@ -4,15 +4,16 @@ import LandingMain from "./LandingMain";
 import RightSidebar from "./RightSidebar";
 import styles from "@/styles/MainLayout.module.css";
 
-export default function MainLayout({childrenEl}){
-
-    return (
-        <>
-            <div className = {`overflow-auto main_layout w-full flex flex-col md:flex-row items-center md:items-start justify-center md:justify-between bg-main_secondary_high h-full`}>
-                <LeftSidebar />
-                {childrenEl}
-                <RightSidebar />
-            </div>
-        </>
-    )
+export default function MainLayout({ childrenEl }) {
+  return (
+    <>
+      <div
+        className={`overflow-auto main_layout w-full flex flex-col md:flex-row items-center md:items-start justify-center md:justify-between bg-main_secondary_high h-full`}
+      >
+        <LeftSidebar />
+        {childrenEl}
+        <RightSidebar />
+      </div>
+    </>
+  );
 }
