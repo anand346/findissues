e57ef6c0deed4069740eddedae6b136f1541a593
Lokesh Veerappan <lokesh.veerappan.47@gmail.com>
2023-10-06 23:27:18 +0530

    fix: update time from now calculation to be more accurate

--- utils/getTimeFromNow.js
+++ utils/getTimeFromNow.js
@@ -1,11 +1,48 @@
 import moment from "moment/moment";
 
 export function getTimeFromNow(created_at) {
-  var timestamp = created_at.split("T");
-  var date = timestamp[0];
-  var time = timestamp[1].split("Z")[0];
-  var finalTime = date + " " + time;
+  const timeDifferenceInMilliseconds = Math.abs(
+    moment
+      .utc(created_at, "YYYY-MM-DD HH:mm:ss")
+      .diff(moment.now(), "milliseconds", true),
+  );
 
-  var finalTimeFromNow = moment.utc(finalTime, "YYYY-MM-DD HH:mm:ss").fromNow();
+  let finalTimePrefix = "a few";
+  let finalTimeUnit = "seconds";
+
+  const timeDifferenceInSeconds = Math.floor(
+    timeDifferenceInMilliseconds / 1000,
+  );
+  const timeDifferenceInMinutes = Math.floor(timeDifferenceInSeconds / 60);
+  const timeDifferenceInHours = Math.floor(timeDifferenceInMinutes / 60);
+  const timeDifferenceInDays = Math.floor(timeDifferenceInHours / 24);
+  const timeDifferenceInWeeks = Math.floor(timeDifferenceInDays / 7);
+  const timeDifferenceInMonths = Math.floor(timeDifferenceInDays / 30);
+  const timeDifferenceInYears = Math.floor(timeDifferenceInDays / 365);
+
+  if (timeDifferenceInYears >= 1) {
+    finalTimePrefix = timeDifferenceInYears;
+    finalTimeUnit = timeDifferenceInYears === 1 ? "year" : "years";
+  } else if (timeDifferenceInMonths >= 1) {
+    finalTimePrefix = timeDifferenceInMonths;
+    finalTimeUnit = timeDifferenceInMonths === 1 ? "month" : "months";
+  } else if (timeDifferenceInWeeks >= 1) {
+    finalTimePrefix = timeDifferenceInWeeks;
+    finalTimeUnit = timeDifferenceInWeeks === 1 ? "week" : "weeks";
+  } else if (timeDifferenceInDays >= 1) {
+    finalTimePrefix = timeDifferenceInDays;
+    finalTimeUnit = timeDifferenceInDays === 1 ? "day" : "days";
+  } else if (timeDifferenceInHours >= 1) {
+    finalTimePrefix = timeDifferenceInHours;
+    finalTimeUnit = timeDifferenceInHours === 1 ? "hour" : "hours";
+  } else if (timeDifferenceInMinutes >= 1) {
+    finalTimePrefix = timeDifferenceInMinutes;
+    finalTimeUnit = timeDifferenceInMinutes === 1 ? "minute" : "minutes";
+  } else if (timeDifferenceInSeconds > 10) {
+    finalTimePrefix = timeDifferenceInSeconds;
+  }
+
+  let finalTimeFromNow = `${finalTimePrefix} ${finalTimeUnit} ago`;
+
   return finalTimeFromNow;
 }
