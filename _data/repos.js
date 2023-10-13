4ce1d3c9fc086f7c54dccc1d01100332843d1e36
Saksham Bhugra <85192629+sh4d0wy@users.noreply.github.com>
2023-10-13 23:32:07 +0530

    chore: Added Chimoney

--- _data/repos.js
+++ _data/repos.js
@@ -2,5 +2,6 @@ export const repos_list = [
     'https://github.com/anand346/findissues',
     'https://github.com/up-for-grabs/up-for-grabs.net',
     'https://github.com/rupali-codes/linkshub',
-    'https://github.com/rohitdasu/projectmate'
+    'https://github.com/rohitdasu/projectmate',
+    'https://github.com/Chimoney/chimoney-community-projects'
 ]
