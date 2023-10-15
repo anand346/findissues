c1e5490bb9b9dfec3f4482cf991e416e579beef4
Anand Raj <64061582+anand346@users.noreply.github.com>
2023-10-15 08:17:32 +0530

    Revert "Updated repos.js" (#77)
    * Revert "Updated repos.js (#73) - build"
    
    This reverts commit 203bc05aab99207dcd1e52d7631e057299a03633.
    
    * Update repos.js - build

--- _data/repos.js
+++ _data/repos.js
@@ -1,8 +1,8 @@
 export const repos_list = [
     'https://github.com/anand346/findissues',
     'https://github.com/up-for-grabs/up-for-grabs.net',
-    'https://github.com/ToolJet/ToolJet'
     'https://github.com/vatsalsinghkv/easy-fix',
+    'https://github.com/ToolJet/ToolJet',
     'https://github.com/rupali-codes/linkshub',
     'https://github.com/rohitdasu/projectmate',
     'https://github.com/freeCodeCamp/freeCodeCamp',
