203bc05aab99207dcd1e52d7631e057299a03633
Pratibhanu Jarngal <99665583+Night-Amber3301@users.noreply.github.com>
2023-10-14 22:42:49 -0400

    Updated repos.js (#73) - build
    * active repo UI initiative
    
    * improved UI
    
    * fetched repo and its issues
    
    * added active repo for mobile
    
    * added list-active-repo file and updated readme
    
    * Update repos.js
    
    solves : #70 
    
    Added an active repo link
    
    ---------
    
    Co-authored-by: = <=>

--- _data/repos.js
+++ _data/repos.js
@@ -1,6 +1,7 @@
 export const repos_list = [
     'https://github.com/anand346/findissues',
     'https://github.com/up-for-grabs/up-for-grabs.net',
+    'https://github.com/ToolJet/ToolJet'
     'https://github.com/vatsalsinghkv/easy-fix',
     'https://github.com/rupali-codes/linkshub',
     'https://github.com/rohitdasu/projectmate',
