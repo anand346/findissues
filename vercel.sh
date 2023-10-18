1675777b07b22cb2b0e067a5be29db05e5bf792c
= <=>
2023-10-18 08:12:50 +0530

    updated build script

--- vercel.sh
+++ vercel.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-if [[ $VERCEL_GIT_COMMIT_REF == "main" && $VERCEL_GIT_REPO_OWNER == "anand346"  ]] ; then 
-  echo "repo owner: $VERCEL_GIT_REPO_OWNER"
+if [[ $VERCEL_GIT_COMMIT_REF == "main" && $VERCEL_GIT_COMMIT_AUTHOR_LOGIN == "anand346"  ]] ; then 
+  echo "repo owner: $VERCEL_GIT_COMMIT_AUTHOR_LOGIN"
+  exit 1
   git log -1 --pretty=oneline --abbrev-commit | grep -w "build" && exit 1 || exit 0
 else
   echo "repo owner: $VERCEL_GIT_REPO_OWNER" 
