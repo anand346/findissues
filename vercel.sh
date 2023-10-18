66eee9dfeca908cefa8a9605758bd81f12cf1bda
= <=>
2023-10-18 08:15:01 +0530

    updated building script

--- vercel.sh
+++ vercel.sh
@@ -5,6 +5,6 @@ if [[ $VERCEL_GIT_COMMIT_REF == "main" && $VERCEL_GIT_COMMIT_AUTHOR_LOGIN == "an
   exit 1
   git log -1 --pretty=oneline --abbrev-commit | grep -w "build" && exit 1 || exit 0
 else
-  echo "repo owner: $VERCEL_GIT_REPO_OWNER" 
+  echo "repo owner: $VERCEL_GIT_COMMIT_AUTHOR_LOGIN" 
   exit 1
 fi
