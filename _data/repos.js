007740468b9eacfecdb72fed3e8c5c8c3751e283
Yash Suthar <98970491+Yash636261@users.noreply.github.com>
2023-10-20 11:05:21 +0530

    added more repos (#100)
    * added more repos
    
    * Update repos.js
    
    * Update repos.js
    
    ---------
    
    Co-authored-by: Anand Raj <64061582+anand346@users.noreply.github.com>

--- _data/repos.js
+++ _data/repos.js
@@ -21,6 +21,8 @@ export const repos_list = [
     'https://github.com/calcom/cal.com',
     'https://github.com/novuhq/novu',
     'https://github.com/camunda/camunda-modeler',
+    'https://github.com/mui/mui-toolpad',
+    'https://github.com/shyamtawli/devFind',
     'https://github.com/developer-student-clubs/npmBox',
     'https://github.com/OtacilioN/awesome-hacktoberfest',
 ]
