3e40037857d9008f2d3a8fe00239b2afe72f9d1d
ShivamGupta <109647722+the-shivam-gupta@users.noreply.github.com>
2023-12-07 12:32:37 +0000

    Deleted the hacktoberfest tag and added a bug tag.

--- helper/tags.js
+++ helper/tags.js
@@ -1,12 +1,12 @@
 export const tags = [
   {
-    tag_name: "#hacktoberfest",
-    query: "hacktoberfest",
-  },
-  {
     tag_name: "#good-first-issue",
     query: "good-first-issue",
   },
+  {
+    tag_name: "#Bugs",
+    query: "bug",
+  }
   // {
   // +label:up-for-grabs+label:good-first-bug+label:easy+label:beginner+label:starter+label:first-timers-only
   //     tag_name : "good-first-issue",
