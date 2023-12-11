90eb0345e3344c6eafbc1483e6eadd36dba33f55
Artal44 <arthur.dandoy02@gmail.com>
2023-12-12 00:44:55 +0100

    Adding of some keywords in SeoTags and adding some meta in _app.js + changing alt of some images

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,31 @@ import Script from "next/script";
 import GeneralLayout from "@/components/GeneralLayout";
 import { ThemeProvider } from "next-themes";
 import Head from "next/head";
+
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>FindIssues</title>
+        <title>FindIssues - Explore Open Source Projects on GitHub</title>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.findissues.me" />
+        <meta name="title" content="FindIssues" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="keywords" content="FindIssues, Github, Hacktoberfest, Open Source, GithubIssues, libraries, frameworks, applications, websites, Python, C, PHP, 
+        Go, Javascript, typescript, HTML, CSS, Java, C++, C#, Rust, Scala, Ruby, Dart, Kotlin, Lua, Swift, Shell, Matlab, Perl, good-first-issues, repo" />
+        <meta name="author" content="Anand Raj" />
+        <meta name="robots" content="index, follow" />
+        <meta name="revisit-after" content="7 days" />
+        <meta name="robots" content="index,follow" />
+        <meta property="og:title" content="FindIssues - Explore Open Source Projects on GitHub" />
+        <meta property="og:description" content="Explore open source projects on GitHub and find relevant issues based on the labels you choose to contribute to. 
+        Discover opportunities for collaboration and learning."/>
+        <meta property="og:image" content="/images/og-image.jpg" />
+        <meta property="og:image:alt" content="FindIssues" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content="/images/og-image.jpg" />
       </Head>
+
       <ThemeProvider defaultTheme="dark">
         <GeneralLayout>
           <Component {...pageProps} />
@@ -27,11 +46,10 @@ export default function App({ Component, pageProps }) {
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-            page_path: window.location.pathname,
+              page_path: window.location.pathname,
             });
         `}
       </Script>
-      <Script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js" />
     </>
   );
 }
