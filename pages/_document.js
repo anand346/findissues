35394da1dcc922e2b0c5cc67725e738a0dad443b
Artal44 <arthur.dandoy02@gmail.com>
2023-12-16 16:24:18 +0100

    modif en fct du PR reject

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta charSet="UTF-8" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
