446413e2925bea632799378fb88d85b6338f0e0f
Priyanshu Kumawat <priyanshukumawat434@gmail.com>
2024-06-18 11:43:28 +0530

    refactor: move loadActiveRepoDeatils function to its own file in api directory (#140)
    Co-authored-by: Anand Raj <64061582+anand346@users.noreply.github.com>

--- pages/active-repos/index.js
+++ pages/active-repos/index.js
@@ -6,8 +6,9 @@ import { repos } from "@/helper/repo";
 import { useState, useEffect, useRef } from "react";
 import { repos_list } from '@/_data/repos';
 import moment from "moment/moment";
-import { BsChevronUp } from "react-icons/bs";
 
+import { BsChevronUp } from "react-icons/bs";
+import loadActiveRepoDetails from "../api/loadActiveRepoDetails";
 export default function ActiveRepo({repoDetails}){
     const { resolvedTheme } = useTheme();
     const theme = resolvedTheme || "dark";
@@ -87,6 +88,7 @@ export default function ActiveRepo({repoDetails}){
                         }
                     })
                 }
+
                 {showScrollTopButton && (
                     <div className="w-full flex justify-end items-center">
                         <button
@@ -106,31 +108,7 @@ export default function ActiveRepo({repoDetails}){
 
 export async function getStaticProps() {
 
-    let repo_result = [];
-
-    repo_result = repos_list.map(async repo_url => {
-        const repo_name = repo_url.split("/")[3] + "/" + repo_url.split("/")[4];
-        const repo_endpoint = 'https://api.github.com/repos/' + repo_name;
-
-        const response = await fetch(repo_endpoint, {
-            headers: {
-                Authorization: "token " + process.env.NEXT_PUBLIC_FETCH_REPO,
-                Accept: "application/vnd.github.v3+json",
-            },
-        });
-        const result = await response.json();
-        const repo_details = {
-            full_name: result.full_name,
-            updated_at: result.updated_at,
-            stars: result.stargazers_count,
-            forks: result.forks_count,
-            language: result.language,
-            open_issues: result.open_issues_count,
-            issue_url: repo_endpoint + '/issues'
-        }
-
-        return repo_details;
-    })
+    let repo_result = await loadActiveRepoDetails(repos_list);
 
     return {
         props: {
