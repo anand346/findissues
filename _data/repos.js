c477785f0480445e2a1c4caed8f76f51f709f687
hayhayes <91683104+hayhayes@users.noreply.github.com>
2025-02-09 12:06:16 -0500

    chore: added turborepo-template (#168)
    Co-authored-by: Anand Raj <64061582+anand346@users.noreply.github.com>

--- _data/repos.js
+++ _data/repos.js
@@ -55,5 +55,6 @@ export const repos_list = [
     'https://github.com/scribe-org/Scribe-Data',
     'https://github.com/heyxyz/hey',
     'https://github.com/ddev/ddev',
+    'https://github.com/react18-tools/turborepo-template',
     'https://github.com/hoppscotch/hoppscotch'
 ]
